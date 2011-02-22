02738351b217e5c07b6efa4a1dd3afbbd9930cfa
Jussi Toivola <jussi.toivola@mfabrik.com>
2011-02-22 13:15:40 +0200

    Added url options for detecting mobile browser

--- tests/test_detectmobilebrowser.js
+++ tests/test_detectmobilebrowser.js
@@ -1,16 +1,31 @@
 /** Tests for detectmobilebrowser.js
  */
 var assert = require('assert');
-var detect = require("../js/detectmobilebrowser");
+var jsdom  = require('jsdom').jsdom
+
+global.window = jsdom().createWindow();
+global.jQuery = require("jquery");
+
+
+var mobilize = require("../js/mobilize.js").mobilize;
 
 function testDetect(expect, name) {
 	var detected;
-	detected = detect.isMobileBrowser(name);
+	detected = mobilize.isMobileBrowser({name : name});
 	assert.equal(expect, detected, name);
 }
 
-testDetect(false, "Mozilla/5.0 (X11; U; Linux x86_64; en-US) AppleWebKit/534.13 (KHTML, like Gecko) Chrome/9.0.597.98 Safari/534.13");
+var browserAgent = "Mozilla/5.0 (X11; U; Linux x86_64; en-US) AppleWebKit/534.13 (KHTML, like Gecko) Chrome/9.0.597.98 Safari/534.13";
+
+testDetect(false, browserAgent);
 testDetect(true, "symbian");
 testDetect(true, "android");
+
+
+detected = mobilize.isMobileBrowser({name : "symbian", force : false});
+assert.equal(false, detected);
+
+detected = mobilize.isMobileBrowser({name : browserAgent, force : true});
+assert.equal(true, detected);
 
 console.log("test_detectmobilebrowser.js passed");
