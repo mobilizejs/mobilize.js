1a4d41869fafa5aaf5ef3f88720850bbab9d491c
Jussi Toivola <jussi.toivola@mfabrik.com>
2011-02-24 19:51:18 +0200

    script for generating api doc with jsdoc

---
+++ gendocs.sh
@@ -0,0 +1,9 @@
+java -jar jsdoc-toolkit/jsrun.jar jsdoc-toolkit/app/run.js ./js  -x=js,jsx --directory=./docs --template=jsdoc-toolkit/templates/jsdoc
+
+# for sphinx
+#java -jar jsdoc-toolkit/jsrun.jar jsdoc-toolkit/app/run.js ./js  -x=js,jsx --directory=./docs/source
+# --template=jsdoc-for-sphinx
+#cd docs
+#make html
+#cd ..
+
