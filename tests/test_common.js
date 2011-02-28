61058ed53e29b7aba7ab64f136c6592b2a354ba7
Jussi Toivola <jussi.toivola@mfabrik.com>
2011-02-28 16:05:44 +0200

    Fixed parsing of URL variables if there's #-character

--- tests/test_common.js
+++ tests/test_common.js
@@ -9,6 +9,16 @@ global.document = {
 
 var mobilize = require("../js/mobilize").mobilize;
 
+function test_getUrlVars() {
+    var vars = mobilize.getUrlVars("http://test?t=2&a=3&b=5")
+    assert.equal("2", vars.t);
+    assert.equal("5", vars.b);
+    
+    var vars = mobilize.getUrlVars("http://test?t=2&a=3&b=5#page-href")
+    assert.equal("2", vars.t);
+    assert.equal("5", vars.b);
+    
+}
 function test_baseurl() {
 	var url = "http://localhost:8080/test/template.html/?testing=123";
 	var expect = "http://localhost:8080/test/";
@@ -29,6 +39,7 @@ function test_url_parameter_add()
 
 
 test_baseurl();
+test_getUrlVars();
 test_url_parameter_add();
 
 console.log("common tests passed");
